07963d4fef5ff35ad290cec12b41385b5f7dfb11
Danny Lucas <dannylucas@MacBook-Pro-van-Danny.local>
2018-01-11 13:39:11 +0100

    no message

--- src/dbManager/queries/userQueries.js
+++ src/dbManager/queries/userQueries.js
@@ -3,8 +3,9 @@ const pool = require('../connection');
 const usersPerGroupView = () => new Promise((resolve, reject) => pool.query('SELECT * FROM usersPerGroupView', (err, res) => err ? reject(err) : resolve(res)));
 
 const insertUserSP = pairs => new Promise((resolve, reject) => {
-  const q = 'CALL SP_InsertIntoAccounten(?, ?, ?, ?, ?, ?, @accountId, ?); SELECT @accountId;';
-  return pool.query(q, Array.from(Object.values(pairs)), (err, res) => err ? reject(err) : resolve(res));
+  const q = 'CALL SP_InsertIntoAccounten(?, ?, ?, ?, ?, ?, ?, @accountId); SELECT @accountId;';
+  const v = Array.from(Object.values(pairs));
+  return pool.query(q, v, (err, res) => err ? reject(err) : resolve(res));
 });
 
 module.exports = {
